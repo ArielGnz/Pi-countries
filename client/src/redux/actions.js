dd99078ae42bd450e84262c83c074cc3314df24b
ArielGnz <arielgon2206@gmail.com>
2024-05-24 20:36:41 -0300

    probando

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -11,7 +11,7 @@ import {
 
 import axios from 'axios';
 
-axios.defaults.baseURL = "radiant-communication-production.up.railway.app";
+axios.defaults.baseURL = "https://radiant-communication-production.up.railway.app/";
 //axios.defaults.baseURL = "http://localhost:3001";
 
 export const getCountries = () => {
@@ -75,26 +75,26 @@ export const poblationOrder = (order) => {
     }
 }
 
-// export function getActivities() {
-//     return async function (dispatch) {
-//             const response = await axios(`/activities`);
-//             return dispatch({
-//                 type: GET_ACTIVITIES,
-//                 payload: response.data
-//             })
+export function getActivities() {
+    return async function (dispatch) {
+            const response = await axios(`/activities`);
+            return dispatch({
+                type: GET_ACTIVITIES,
+                payload: response.data
+            })
        
-//     }
-// }
+    }
+}
 
 
-// export function postForm(payload) {
+export function postForm(payload) {
 
-//     return async function () {
-//         try {
-//             const res = await axios.post('/activities', payload)
-//             return res;
-//         } catch (error) {
+    return async function () {
+        try {
+            const res = await axios.post('/activities', payload)
+            return res;
+        } catch (error) {
             
-//         }
-//     }
-// }
+        }
+    }
+}
